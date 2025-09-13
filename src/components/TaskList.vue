79e911bfcffc2ba60a7a23014cef54ee5d6bc027
javi9707 <javierlopezrequejo@gmail.com>
2025-09-13 17:52:40 -0400

    Refactor TaskList to use useFetch composable
    Replaced inline data fetching logic in TaskList.vue with a reusable useFetch composable for improved code organization and reusability. Added useFetch.ts to encapsulate async data fetching, loading, and error state management.

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,47 +1,39 @@
-    <template>
-        <div class="q-pa-md" style="max-width: 350px">
+<template>
+    <div class="q-pa-md" style="max-width: 350px">
+
         <div class="q-pa-md q-gutter-sm">
             <q-input outlined v-model="newTask" label="Outlined" />
-            <q-btn color="primary" label="Primary" />
+            <q-btn color="primary" label="New Task" />
         </div>
-        <q-list v-if="tasks.length > 0" bordered separator>
+
+        <q-spinner v-if="loading" color="primary" size="30px" />
+
+        <p v-if="error" class="text-negative">{{ error }}</p>
+
+        <q-list v-else-if="tasks?.length" bordered separator>
             <q-item v-for="task in tasks" :key="task.id" clickable v-ripple>
-            <q-item-section>{{ task.title }}</q-item-section>
-            <q-item-section>{{ task.is_done }}</q-item-section>
-            <q-item-section>{{ task.keywords }}</q-item-section>
+                <q-item-section>{{ task.title }}</q-item-section>
+                <q-item-section>{{ task.is_done }}</q-item-section>
+                <q-item-section>{{ task.keywords }}</q-item-section>
             </q-item>
         </q-list>
         <p v-else>No tasks</p>
 
-        </div>
-    </template>
+    </div>
+</template>
 
-    <script setup lang="ts">
-    import { ref, onMounted } from 'vue'
-    import axios from 'axios'
-    import type { Task } from '../types'
 
-    const tasks = ref<Task[]>([])
-    const loading = ref(false)
-    const error = ref(null)
-    const newTask = ref("")
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { Task } from '../types'
+import { useFetch } from '@/composables/useFetch'
 
-    const API_URL = process.env.VUE_APP_BACKEND_API_BASE_URL as string
-    
-    onMounted(() =>{
-        fetchUsers()   
-    })
+const API_URL = process.env.VUE_APP_BACKEND_API_BASE_URL as string
+const { data: tasks, loading, error, fetchData } = useFetch<Task[]>(`${API_URL}/tasks`)
 
-    const fetchUsers = async () => {
-    loading.value = true
-    error.value = null
-    try {
-        const response = await axios.get<Task[]>(`${API_URL}/tasks`)
-        tasks.value = response.data
-    } catch (err) {
-        error.value = 'Error al cargar los usuarios'
-    } finally {
-        loading.value = false
-    }
-    }
-    </script>
+const newTask = ref("")
+
+onMounted(() => {
+    fetchData()
+})
+</script>
